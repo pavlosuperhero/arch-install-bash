837da2cc95ae27c531f793ae4c2b57b4f6287114
pavlosuperhero <pavlo.fabuliak@gmail.com>
2021-01-25 12:28:01 +0000

    Added installation

--- fullinstall.sh
+++ fullinstall.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 pacman -Syyy
-pacman -Syyuu
 pacman -S reflector --noconfirm
 
 reflector -c Ukraine -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 timedatectl set-ntp true
-mkfs.fat -F32 /dev/sda1
-mkfs.btrfs /dev/sda2
+mkfs.vfat -F32 /dev/sda1
+mkfs.btrfs -f /dev/sda2
 
-mount /dev/sda1 /mnt
+mount /dev/sda2 /mnt
 
 btrfs su cr /mnt/@
 btrfs su cr /mnt/@home
@@ -25,3 +24,6 @@ mount -o noatime,compress=lzo,space_cache,ssd,subvol=@home /dev/sda2 /mnt/home
 mount -o noatime,compress=lzo,space_cache,ssd,subvol=@var /dev/sda2 /mnt/var
 mount -o noatime,compress=lzo,space_cache,ssd,subvol=@snapshots /dev/sda2 /mnt/.snapshots
 mount /dev/sda1 /mnt/boot
+
+pacstrap /mnt base linux linux-firmware vim nano snapper
+genfstab -U /mnt >> /mnt/etc/fstab
